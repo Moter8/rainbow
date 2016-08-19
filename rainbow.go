e34f345b90f06dce95fbc630247586da1a471574
Carsten Hagemann <moter8@gmail.com>
2016-08-20 00:39:52 +0200

    Counter added, new messages

--- rainbow.go
+++ rainbow.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/yosssi/ace"
 )
+
+var i = 1
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
@@ -18,14 +21,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	messages := []string{"You are awesome,", "Pure awesomeness", "Coolest person alive:", "Literally Awesome:", "The amazing"}
+	messages := []string{
+		"You are awesome,",
+		"Pure awesomeness",
+		"Coolest person alive:",
+		"Literally Awesome:",
+		"The amazing",
+		"YOLO 420 -",
+		"Knows how to tie their shoes:",
+		"Makes delicious cakes:",
+	}
 	selectedMessage := messages[rand.Intn(len(messages))]
 
-	templateErr := tpl.Execute(w, map[string]string{"Message": selectedMessage, "Url": r.URL.Path[1:]})
+	templateErr := tpl.Execute(w, map[string]string{"Message": selectedMessage, "Counter": strconv.Itoa(i), "Url": r.URL.Path[1:]})
 	if templateErr != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	} else {
 		fmt.Println("Serving " + selectedMessage + " " + r.URL.Path[1:] + " for " + r.RemoteAddr)
+		if r.URL.Path[1:] != "favicon.ico" {
+			i++
+		}
 	}
 }
 
